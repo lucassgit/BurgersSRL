f588fe68a2620cb6b581a469bb2c406627180a0c
lucassgit <lucaseoviedo.92@gmail.com>
2022-10-27 20:53:20 -0300

    cambios

--- app/Entidades/producto.php
+++ app/Entidades/producto.php
@@ -8,49 +8,88 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
 
-    protected $table = 'productos';
-    public $timestamps = false;
+      protected $table = 'productos';
+      public $timestamps = false;
 
-    protected $fillable = [ //Campos en la tabla clientes de la BDD...
-        'idproducto', 'nombre', 'cantidad', 'precio', 'imagen', 'fk_idcategoria',
-    ];
+      protected $fillable = [ //Campos en la tabla clientes de la BDD...
+            'idproducto', 'nombre', 'cantidad', 'precio', 'imagen', 'fk_idcategoria',
+      ];
 
-    protected $hidden = [];
+      protected $hidden = [];
 
-    public function obtenerTodos()
-    {
-        $sql = "SELECT
+      public function obtenerTodos()
+      {
+            $sql = "SELECT
+                  idproducto
                   nombre
                   cantidad
                   precio
                   imagen
                   fk_idcategoria
                 FROM productos ORDER BY nombre ASC";
-        $lstRetorno = DB::select($sql);
-        return $lstRetorno;
-    }
+            $lstRetorno = DB::select($sql);
+            return $lstRetorno;
+      }
 
-    public function obtenerPorId($idproducto)
-    {
-        $sql = "SELECT
+      public function obtenerPorId($idproducto)
+      {
+            $sql = "SELECT
+                  idproducto
                   nombre
                   cantidad
                   precio
                   imagen
                   fk_idcategoria
                 FROM productos WHERE idproducto = $idproducto";
-        $lstRetorno = DB::select($sql);
+            $lstRetorno = DB::select($sql);
 
-        if (count($lstRetorno) > 0) {
-            $this->idproducto = $lstRetorno[0]->idproducto;
-            $this->nombre = $lstRetorno[0]->nombre;
-            $this->cantidad = $lstRetorno[0]->cantidad;
-            $this->precio = $lstRetorno[0]->precio;
-            $this->imagen = $lstRetorno[0]->imagen;
-            $this->fk_idcategoria = $lstRetorno[0]->fk_idcategoria;
-            return $this;
-        }
-        return null;
-    }
+            if (count($lstRetorno) > 0) {
+                  $this->idproducto = $lstRetorno[0]->idproducto;
+                  $this->nombre = $lstRetorno[0]->nombre;
+                  $this->cantidad = $lstRetorno[0]->cantidad;
+                  $this->precio = $lstRetorno[0]->precio;
+                  $this->imagen = $lstRetorno[0]->imagen;
+                  $this->fk_idcategoria = $lstRetorno[0]->fk_idcategoria;
+                  return $this;
+            }
+            return null;
+      }
 
+      public function guardar()
+      {
+            $sql = "UPDATE postulaciones SET
+            nombre='$this->nombre',
+            cantidad='$this->cantidad',
+            precio='$this->precio',
+            imagen='$this->imagen',
+            fk_idcategoria='$this->fk_idcategoria',
+            WHERE idproducto=?";
+            $affected = DB::update($sql, [$this->idproducto]);
+      }
+
+      public function eliminar()
+      {
+            $sql = "DELETE FROM productos WHERE
+            idproducto=?";
+            $affected = DB::delete($sql, [$this->idproducto]);
+      }
+
+      public function insertar()
+      {
+            $sql = "INSERT INTO productos (
+                  nombre
+                  cantidad
+                  precio
+                  imagen
+                  fk_idcategoria
+            ) VALUES (?, ?, ?, ?, ?);";
+            $result = DB::insert($sql, [
+                  $this->nombre,
+                  $this->cantidad,
+                  $this->precio,
+                  $this->imagen,
+                  $this->fk_idcategoria,
+            ]);
+            return $this->idproducto = DB::getPdo()->lastInsertId();
+      }
 }
